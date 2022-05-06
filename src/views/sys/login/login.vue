3c57008f9739fc6265c5b4cde43fa23d9ccf7b97
lqr131115 <1046975211@qq.com>
2022-05-06 17:52:31 +0800

    feat: login页适配

--- src/views/sys/login/login.vue
+++ src/views/sys/login/login.vue
@@ -2,25 +2,52 @@
   <div class="login">
     <div class="login__left">
       <div class="left__wrapper">
-        <span class="text_1">Call Me <span>Ares</span></span>
-        <img class="login-box-bg" src="@/icons/svg/login-box-bg.svg" alt="">
-        <span class="text_2">一个简约的、开箱即用的后台管理系统解决方案</span>
-        <p fz16>我自幼向往水乡风光 可我从未去过江南</p>
-        <p fz16>你说你是姑苏人 在我看来</p>
-        <p fz16>你和那到不了的江南一样</p>
+        <div class="content">
+          <span class="text_1">
+            Call Me
+            <span>Ares</span>
+          </span>
+          <div class="logo">
+            <img class="login-box-bg" src="@/icons/svg/login-box-bg.svg" alt="logo" />
+          </div>
+          <div class="desc">
+            <span class="text_2">一个简约的、开箱即用的后台管理系统解决方案</span>
+          </div>
+          <div class="miss-xwt">
+            <p fz16>我自幼向往水乡风光 可我从未去过江南</p>
+            <p fz16>你说你是姑苏人 在我看来</p>
+            <p fz16>你和那到不了的江南一样</p>
+          </div>
+        </div>
       </div>
     </div>
     <div class="login__right">
       <el-card shadow="hover">
         <h2>用户登录</h2>
-        <el-form ref="loginRef" class="login__form" label-width="100px" :model="loginForm" :rules="loginRules"
-          hide-required-asterisk label-position="top">
+        <el-form
+          ref="loginRef"
+          class="login__form"
+          label-width="100px"
+          :model="loginForm"
+          :rules="loginRules"
+          hide-required-asterisk
+          label-position="top"
+        >
           <el-form-item label="Username" prop="username">
-            <el-input v-model="loginForm.username" placeholder="用户名为admin或者super" :prefix-icon="User" />
+            <el-input
+              v-model="loginForm.username"
+              placeholder="用户名为admin或者super"
+              :prefix-icon="User"
+            />
           </el-form-item>
           <el-form-item label="Password" prop="password">
-            <el-input v-model="loginForm.password" type="password" placeholder="密码为123123" :prefix-icon="Lock"
-              show-password />
+            <el-input
+              v-model="loginForm.password"
+              type="password"
+              placeholder="密码为123123"
+              :prefix-icon="Lock"
+              show-password
+            />
           </el-form-item>
           <el-form-item>
             <div class="item__remember">
@@ -29,17 +56,21 @@
             </div>
           </el-form-item>
           <el-form-item>
-            <el-button type="primary" :loading="loginLoading" @click="submitForm(loginRef)" :style="{ width: '100%' }">
-              登录</el-button>
+            <el-button
+              type="primary"
+              :loading="loginLoading"
+              @click="submitForm(loginRef)"
+              :style="{ width: '100%' }"
+            >登录</el-button>
           </el-form-item>
         </el-form>
         <div mb10>
           <OtherSignIn />
         </div>
-        </el-card>
+      </el-card>
       <!-- <div class="icp">
         <el-link :underline="false" href="https://beian.miit.gov.cn" target="_blank">皖ICP备2022006209号</el-link>
-      </div> -->
+      </div>-->
     </div>
   </div>
 </template>
@@ -130,30 +161,76 @@ const _login = () => {
     color: #fff;
 
     .left__wrapper {
-      @include box-center(start, start);
-      flex-direction: column;
+      @include box-center(center, center);
       height: 100%;
+      .content {
+        @include box-center(space-around, flex-start);
+        @include dimensions(50%, 50%);
+        flex-direction: column;
 
-      .text_1 {
-        font-size: 30px;
-        font-weight: bold;
+        .text_1 {
+          font-size: 30px;
+          font-weight: bold;
 
-        span {
-          font-size: 50px;
-          font-weight: bolder;
+          span {
+            font-size: 50px;
+            font-weight: bolder;
+          }
         }
 
-        margin: 90px 0 60px;
-      }
+        .logo {
+          @include box-center(flex-start, center);
+        }
+        .desc {
+          @include box-center(center, flex-start);
+          flex-direction: column;
+          text-align: left;
 
-      .text_2 {
-        font-size: 20px;
-        margin: 40px 0 30px;
+          .text_2 {
+            font-size: 20px;
+            font-weight: bold;
+
+            & {
+              text-align: center;
+              background-image: -webkit-linear-gradient(
+                left,
+                #6dfdd9,
+                #ffffff 25%,
+                #6dfdd9 50%,
+                #ffffff 75%,
+                #6dfdd9
+              );
+              -webkit-text-fill-color: transparent;
+              -webkit-background-clip: text;
+              -webkit-background-size: 200% 100%;
+              -webkit-animation: miss 3s infinite linear;
+            }
+
+            @-webkit-keyframes miss {
+              0% {
+                background-position: 0 0;
+              }
+
+              100% {
+                background-position: -100% 0;
+              }
+            }
+          }
+        }
+        .miss-xwt {
+          @include box-center(center, flex-start);
+          flex-direction: column;
+          text-align: left;
+          p {
+            line-height: 2em;
+            font-weight: 600;
+          }
+        }
       }
     }
 
     .login-box-bg {
-      width: 320px;
+      width: 80%;
     }
   }
 
@@ -185,11 +262,11 @@ const _login = () => {
     width: 100%;
     height: 100%;
     margin-left: -48%;
-    background-image: url('@/icons/svg/login-bg.svg');
+    background-image: url("@/icons/svg/login-bg.svg");
     background-position: 100%;
     background-repeat: no-repeat;
     background-size: auto 100%;
-    content: '';
+    content: "";
   }
 }
 </style>
